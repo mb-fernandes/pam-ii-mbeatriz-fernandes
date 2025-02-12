6bc1aacedaa10213ca0f615d5a1fd1b33b76cbbf
Maria Beatriz <fernandeslopes16@hotmail.com>
2025-02-12 08:40:23 -0300

    ATIVIDADE NSA

---
+++ NSA-Projeto/app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+import { StyleSheet } from "react-native";
+
+export default function Index() {
+  return ( 
+    <View style={styles.container}>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#141414',
+  }})
